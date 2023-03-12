9700beaf86a3e1247448e0307593a3d1bd087374
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2023-03-12 19:05:02 +0100

    Fix Movepdf in vimtex config

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -19,16 +19,6 @@ vim.g.vimtex_compiler_latexmk = {
 	build_dir = ".build",
 }
 vim.g.vimtex_quickfix_open_on_warning = false
--- vim.g.vimtex_latexmk_options = "-shell-escape -verbose -file-line-error -synctex=1 -interaction=nonstopmode"
--- vim.g.vimtex_compiler_latexmk = "lualatex"
--- vim.cmd([[ syntax enable ]])
--- vim.g.vimtex_compiler_latexmk_engines = {
--- 	_        = '-lualatex',
--- 	pdflatex = '-pdf',
--- 	dvipdfex = '-pdfdvi',
--- 	lualatex = '-lualatex',
--- 	xelatex  = '-xelatex'
--- }
 
 local api = vim.api
 
@@ -37,16 +27,23 @@ local function FocusVim()
 end
 
 local function MovePdf() -- Move pdf
-	local path = vim.b.vimtex.tex
-	local base_start, base_end = string.find(path, "[^/]*[.]tex")
-	base_end = base_end - 4 -- ignore the ".tex"
-	local base = string.sub(path, base_start, base_end)
-	local dir = string.sub(path, 1, base_start - 1)
-	local pdf = dir .. ".build/" .. base .. ".pdf"
-	local new_pdf = dir .. base .. ".pdf"
-	local pdf_file = vim.fn.readfile(pdf, "b")
-	vim.fn.writefile(pdf_file, new_pdf, "b")
-	vim.cmd(string.format([[call vimtex#log#info('Moved %s out of .build')]], pdf))
+	local project_root = vim.b.vimtex.root
+	local pdf = project_root .. "/.build/main.pdf"
+	local new_pdf = project_root .. "/main.pdf"
+	if vim.fn.filereadable(pdf) then
+		vim.fn.writefile(vim.fn.readfile(pdf, "b"), new_pdf, "b")
+		vim.cmd(string.format([[call vimtex#log#info('Moved %s out of .build')]], pdf))
+	end
+	-- local path = vim.b.vimtex.tex
+	-- local base_start, base_end = string.find(path, "[^/]*[.]tex")
+	-- base_end = base_end - 4 -- ignore the ".tex"
+	-- local base = string.sub(path, base_start, base_end)
+	-- local dir = string.sub(path, 1, base_start - 1)
+	-- local pdf = dir .. ".build/" .. base .. ".pdf"
+	-- local new_pdf = dir .. base .. ".pdf"
+	-- local pdf_file = vim.fn.readfile(pdf, "b")
+	-- vim.fn.writefile(pdf_file, new_pdf, "b")
+	-- vim.cmd(string.format([[call vimtex#log#info('Moved %s out of .build')]], pdf))
 end
 
 local VimTexAugroup = api.nvim_create_augroup("VimTex", { clear = true })
