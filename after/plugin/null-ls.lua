e4e7081f99f496b8cf469694c705d03aae288381
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2023-04-07 12:18:18 +0200

    Add prettier formatter for markup languages

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -4,6 +4,7 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.ocamlformat,
+        null_ls.builtins.formatting.prettier,
     },
 })
 
