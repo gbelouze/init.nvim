72d657b4328d65334ec4a3e2fa771078f9c3078e
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2023-05-10 09:32:19 +0200

    Shortcut to jump to the next Luasnip placeholder

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -2,20 +2,24 @@ local loader = require("luasnip.loaders.from_lua")
 local ls = require("luasnip")
 
 loader.load({
-    paths = "~/.config/nvim/snippets",
+	paths = "~/.config/nvim/snippets",
 })
 
 ls.config.set_config({
-    enable_autosnippets = true,
-    store_selection_keys = "<Tab>",
-    update_events = "TextChanged,TextChangedI",
+	enable_autosnippets = true,
+	store_selection_keys = "<Tab>",
+	update_events = "TextChanged,TextChangedI",
 })
 
 vim.keymap.set(
-    "n",
-    "<Leader>L",
-    '<Cmd>lua require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/snippets/"})<CR>',
-    {
-        desc = "Reload the snippets",
-    }
+	"n",
+	"<Leader>L",
+	'<Cmd>lua require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/snippets/"})<CR>',
+	{
+		desc = "Reload the snippets",
+	}
 )
+
+vim.keymap.set("i", "<C-d>", "<Plug>luasnip-jump-next", {
+	desc = "Jump to the next Luasnip placeholder",
+})
