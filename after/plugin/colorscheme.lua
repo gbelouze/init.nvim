9ad6e33b1e4cf7f3d1f6a0756fd58049c7466951
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2023-09-12 19:14:09 +0200

    Better colorscheme integration with kitty

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,1 +1,9 @@
-vim.cmd[[colorscheme catppuccin]]
+vim.cmd[[colorscheme catppuccin-frappe]]
+
+vim.api.nvim_create_user_command("Light", function()
+	vim.opt.background = "light"
+end, {})
+vim.api.nvim_create_user_command("Dark", function()
+	vim.opt.background = "dark"
+end, {})
+
