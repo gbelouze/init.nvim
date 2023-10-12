0afe9a1a9f0555fcfc2210bf44c21c829fec6729
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2023-10-12 14:40:00 +0200

    Update neorg config

--- lua/user/plugins/neorg.lua
+++ lua/user/plugins/neorg.lua
@@ -1,16 +1,35 @@
 return {
 	{
 		"nvim-neorg/neorg",
+		dependencies = {
+			"folke/which-key.nvim",
+			"nvim-lua/plenary.nvim",
+		},
 		config = function()
 			require("neorg").setup({
 				load = {
 					["core.defaults"] = {}, -- Loads default behaviour
-					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.concealer"] = {
+						config = {
+							icon_preset = "diamond",
+							folds = true,
+							icons = {
+								todo = {
+									pending = {
+										icon = "",
+									},
+								},
+							},
+						},
+					}, -- Adds pretty icons to your documents
 					["core.dirman"] = { -- Manages Neorg workspaces
 						config = {
 							workspaces = {
 								notes = "~/notes",
+								phd = "~/Documents/phd/notes",
 							},
+							default_workspace = "notes",
+							index = "index.norg",
 						},
 					},
                     ["core.keybinds"] = {
@@ -21,10 +40,43 @@ return {
                             end
                         }
                     }
+                    -- unavailable while nvim 10.0.0 is nightly
+                    -- ["core.tempus"] = {}
 				},
 			})
 		end,
 		-- build = ":Neorg sync-parsers",
-		dependencies = "nvim-lua/plenary.nvim",
+		init = function()
+			local wk = require("which-key")
+			wk.register({
+				["<Leader>"] = {
+					n = {
+						name = "+neorg",
+						i = {
+							function()
+								vim.cmd([[Neorg index]])
+							end,
+							"Neorg index",
+						},
+                        c = {
+                            function ()
+                                vim.cmd([[Neorg toggle-concealer]])
+                            end,
+                            "Neorg toggle-concealer"
+                        },
+						r = {
+							function()
+								vim.cmd([[Neorg return]])
+							end,
+							"Neorg return",
+						},
+                        w = {
+                            ":Neorg workspace ",
+                            "Neorg workspace"
+                        }
+					},
+				},
+			})
+		end,
 	},
 }
