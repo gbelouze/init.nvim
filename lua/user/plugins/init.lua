004951a32be500a8c37a805acc1795da0792cf09
Gabriel Belouze <gabriel@belouze.com>
2024-09-19 18:25:30 +0200

    Formatting

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,20 +1,25 @@
 return {
 
-    -- better nvim
-    { "ggandor/leap.nvim", config = function() require("leap").add_default_mappings() end, }, -- movement
-    { "kylechui/nvim-surround", event = "VeryLazy",                                           opts = {} }, -- delimiters
-    { "numToStr/Comment.nvim",  lazy = false,                                                 opts = {} }, -- comment
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {scope = {enabled = false}} }, -- indentation guides
-    "folke/zen-mode.nvim",
+	-- better nvim
+	{
+		"ggandor/leap.nvim",
+		init = function()
+			require("leap").add_default_mappings()
+		end,
+	}, -- movement
+	{ "kylechui/nvim-surround", event = "VeryLazy", opts = {} }, -- delimiters
+	{ "numToStr/Comment.nvim", lazy = false, opts = {} }, -- comment
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = { scope = { enabled = false } } }, -- indentation guides
+	"folke/zen-mode.nvim",
 
-    -- integration
-    "lifepillar/pgsql.vim", -- postgresql
-    "fladson/vim-kitty", -- kitty
-    "folke/neodev.nvim", -- lua
-    "milisims/nvim-luaref", -- lua
-    "tpope/vim-fugitive", -- git
-    { "lewis6991/gitsigns.nvim",                             enable = false }, -- git
+	-- integration
+	"lifepillar/pgsql.vim", -- postgresql
+	"fladson/vim-kitty", -- kitty
+	"folke/neodev.nvim", -- lua
+	"milisims/nvim-luaref", -- lua
+	"tpope/vim-fugitive", -- git
+	{ "lewis6991/gitsigns.nvim", enable = false }, -- git
 
-    -- Typing exercises
-    -- { dir = "~/Documents/Programmation/Nvim/stereotype.nvim", opts = {data = "python_snippets"} },
+	-- Typing exercises
+	-- { dir = "~/Documents/Programmation/Nvim/stereotype.nvim", opts = {data = "python_snippets"} },
 }
